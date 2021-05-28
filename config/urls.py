c64694c551977e2f4053bbb7abdd54ba20b7f244
RyanRamsey <meyeryan@gmail.com>
2021-05-28 16:59:11 -0600

    handle media when debug is false

--- config/urls.py
+++ config/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+import os
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('notadmin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if os.environ.get('DEBUG'):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
