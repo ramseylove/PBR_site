45e20ce8d33268b452d10f53dfb948b078a55fb7
RyanRamsey <meyeryan@gmail.com>
2021-06-03 22:40:10 -0600

    add myscript.js

---
+++ static/scripts/myScripts.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const tabs = document.querySelectorAll('.v-pills-tab');
+// const tabsContainer = document.querySelector('.tab-content');
+const tabsContent = document.querySelector('.tab-content');
+
