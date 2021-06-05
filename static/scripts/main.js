7f7df039050247ecbf8dfd3de51ed672878f3a17
Ryan Meyer <ryan@atriadev.com>
2021-06-05 15:41:11 -0600

    skills are fixed, moved script into main.js

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -5,3 +5,23 @@ AOS.init({
 });
 
 // Add your javascript here
+
+const tabs = document.querySelectorAll('.pills-tab');
+const tabsContainer = document.querySelector('#v-pills-tablist');
+const tabsContent = document.querySelectorAll('.tab-pane');
+
+// using event delagation requires selection of the parent element of the tabs
+tabsContainer.addEventListener('click',function (e) {
+    //matching strategy using closest
+    const clicked = e.target.closest('#v-pills-tab');
+    console.log(clicked);
+
+    if (!clicked) return;
+    tabs.forEach(t => t.classList.remove('active'))
+    tabsContent.forEach(c => c.classList.remove('show', 'active'))
+
+    clicked.classList.add('active')
+
+    document.querySelector(`#v-pills-${clicked.dataset.tab}`).classList.add('show', 'active');
+
+});
