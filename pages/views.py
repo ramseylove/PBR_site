daa94450e0ff90beac358bf616a39828d24c6ecf
RyanRamsey <meyeryan@gmail.com>
2021-06-15 19:05:41 -0600

    optimized queries, down to 8. Portfolio modal window to be done yet

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
+# from django.contrib.auth import get_user_model
 
-from .models import Resume, SkillsTag
+from .models import Resume, SkillsTag, Portfolio, Skills
+from accounts.models import UserProfile
 
 
 def resume_view(request):
-    resume = Resume.objects.get(pk=1)
-    skill_tags = SkillsTag.objects.all().distinct()
+    resume = Resume.objects.select_related('user__userprofile').get(pk=1)
+    # resume = Resume.objects.get(pk=1)
+    profile = resume.user.userprofile
+    skill_tags = SkillsTag.objects.prefetch_related('tags').all().distinct()
+    portfolio = Portfolio.objects.filter(resume=resume)
+    skills = Skills.objects.prefetch_related('tag').filter(resume=resume)
     context = {
         "resume": resume,
+        "profile": profile,
         "skill_tags": skill_tags,
+        "portfolio": portfolio,
+        "skills": skills,
         "request": request
     }
     print(dir(request))
