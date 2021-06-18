5b7241f4ad1ef625cc67f34f2d149d6fb090272d
RyanRamsey <meyeryan@gmail.com>
2021-06-17 21:51:32 -0600

    fixed portfolio model, working on the portfolio modal

--- pages/views.py
+++ pages/views.py
@@ -7,16 +7,15 @@ from accounts.models import UserProfile
 
 def resume_view(request):
     resume = Resume.objects.select_related('user__userprofile').get(pk=1)
-    # resume = Resume.objects.get(pk=1)
     profile = resume.user.userprofile
     skill_tags = SkillsTag.objects.prefetch_related('tags').all().distinct()
-    portfolio = Portfolio.objects.filter(resume=resume)
+    portfolios = Portfolio.objects.prefetch_related('images').filter(resume=resume)
     skills = Skills.objects.prefetch_related('tag').filter(resume=resume)
     context = {
         "resume": resume,
         "profile": profile,
         "skill_tags": skill_tags,
-        "portfolio": portfolio,
+        "portfolios": portfolios,
         "skills": skills,
         "request": request
     }
