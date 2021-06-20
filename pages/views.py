b15a4001b7bc3a3db61cd9ee1f4c551d023e537c
Ryan Meyer <ryan@atriadev.com>
2021-06-20 15:22:45 -0600

    started portfolio modal view

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.core import serializers
+from django.http import JsonResponse
+from django.views.generic import View
 
-from .models import Resume, SkillsTag
+from .models import Resume, SkillsTag, Portfolio
 
 
 def resume_view(request):
@@ -13,3 +16,11 @@ def resume_view(request):
     }
     print(dir(request))
     return render(request, 'pages/home.html', context)
+
+
+def portfolio_view(request):
+    portfolios = Portfolio.objects.filter(resume_id=1)
+    data = serializers.serialize('json', portfolios)
+    data = JsonResponse({'data': data}, safe=False)
+
+    return render(request, 'pages/home.html', {'data': data})
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -4,14 +4,32 @@ AOS.init({
   duration: 1000
 });
 
-// Add your javascript here
+
 
 const tabs = document.querySelectorAll('.pills-tab');
 const tabsContainer = document.querySelector('#v-pills-tablist');
 const tabsContent = document.querySelectorAll('.tab-pane');
 
+const portfolioModal = document.querySelector('.modalPortfolio')
+
+// portfolio modal //
+
+// $.ajax({
+//     type: 'GET',
+//     url: '/portfolio/',
+//     success: function (response) {
+//         console.log(response)
+//     },
+//     error: function(error) {
+//         console.log(error)
+//     }
+//
+// });
+
+// tab container //
 // using event delagation requires selection of the parent element of the tabs
-tabsContainer.addEventListener('click',function (e) {
+tabsContainer.addEventListener(
+    'click',function (e) {
     //matching strategy using closest
     const clicked = e.target.closest('#v-pills-tab');
     console.log(clicked);
@@ -25,3 +43,4 @@ tabsContainer.addEventListener('click',function (e) {
     document.querySelector(`#v-pills-${clicked.dataset.tab}`).classList.add('show', 'active');
 
 });
+
