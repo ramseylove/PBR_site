94cc395268d8650a0b39f87ca6e38cf654deec70
Ryan Meyer <ryan@atriadev.com>
2021-06-20 15:22:45 -0600

    started portfolio modal view

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import resume_view
+from .views import resume_view, portfolio_view
 
 urlpatterns = [
     path('', resume_view, name='resume'),
+    path('portfolio/', portfolio_view, name='portfolio')
 ]
 
