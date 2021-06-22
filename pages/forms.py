1d064116f1d53f2c97a49e6275c494fede191488
RyanRamsey <meyeryan@gmail.com>
2021-06-21 18:30:13 -0600

    form replaced with crispy form

---
+++ pages/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(required=True)
+    from_email = forms.EmailField(required=True, label='Email Address')
+    # subject = forms.CharField(required=True)
+    message = forms.CharField(required=True, widget=forms.Textarea(attrs={'rows': "4"} ))
+
+    class Meta:
+        fields = ('from_email', 'name', 'message',)
