10c26de389cbe0a9fd533ea2797d8249c5faf0f7
RyanRamsey <meyeryan@gmail.com>
2021-06-22 22:10:59 -0600

    added ajax contact form, with email sent via sendgrid

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -17,27 +17,22 @@ const portfolioModal = document.querySelector('.modalPortfolio')
 const showPortfolio = document.querySelector('#portfolio-link')
 const portfolio = document.querySelector('.portfolio')
 
-// portfolio modal //
-// showPortfolio.addEventListener('click', e=>{
-//     e.preventDefault()
-//
-//     $.ajax({
-//     type: 'GET',
-//     url: '/portfolio/',
-//     success: function (response) {
-//         console.log(response.data)
-//     },
-//     error: function(error) {
-//         console.log(error)
-//     }
-//
-// });
-// })
-//
-// portfolio.addEventListener('hover', function(){
-//     portfolio.classList.add('border border-secondary')
-// })
+const alertBox = document.getElementById('alert-box')
 
+const contactForm = document.querySelector('#contact-form')
+const nameInput = document.querySelector('#id_name')
+const fromEmailInput = document.querySelector('#id_from_email')
+const messageInput = document.querySelector('#id_message')
+const csrf = document.getElementsByName('csrfmiddlewaretoken')
+
+
+// alert box
+const handleAlert = (type, msg) => {
+    alertBox.innerHTML = `
+        <div class="alert alert-${type}" role="alert">
+          ${msg}
+        </div>`
+}
 
 // tab container //
 
@@ -58,6 +53,69 @@ tabsContainer.addEventListener(
     document.querySelector(`#v-pills-${clicked.dataset.tab}`).classList.add('show', 'active');
 
 });
+
+const getCookie = (name) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+contactForm.addEventListener('submit', e=>{
+    e.preventDefault();
+
+    $.ajax({
+        type: 'POST',
+        url: '',
+        data: {
+            'csrfmiddlewaretoken': csrf[0].value,
+            'name': nameInput.value,
+            'from_email': fromEmailInput.value,
+            'message': messageInput.value,
+        },
+        success: function(response){
+            console.log(response)
+            contactForm.classList.add('not-visible')
+            handleAlert('success', 'Email sent')
+        },
+        error: function(error){
+            console.log(error)
+            handleAlert('danger', 'Error sending email')
+        }
+    })
+})
+
+
+    // portfolio modal //
+// showPortfolio.addEventListener('click', e=>{
+//     e.preventDefault()
+//
+//     $.ajax({
+//     type: 'GET',
+//     url: '/portfolio/',
+//     success: function (response) {
+//         console.log(response.data)
+//     },
+//     error: function(error) {
+//         console.log(error)
+//     }
+//
+// });
+// })
+//
+// portfolio.addEventListener('hover', function(){
+//     portfolio.classList.add('border border-secondary')
+// })
 
 
 // portfolioSection.addEventListener('hover', function(e) {
